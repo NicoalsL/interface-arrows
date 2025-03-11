ebad3b52eededebd00d6df6f9c8eeabd1484afd3
NicoalsL <nicolas.lebon2@icloud.com>
2025-03-11 11:35:46 +0100

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from "react";
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <div className="input-container">
+        <input type="text" placeholder="Saisissez votre texte..." />
+      </div>
+    </>
+  );
+}
+
+export default App;
